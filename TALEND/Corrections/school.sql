0a62a2ac53c86db110d952aaa370265865e87b25
Antoine07 <antoine.lucsko@gmail.com>
2022-12-12 23:47:37 +0100

    cours

--- TALEND/Corrections/school.sql
+++ TALEND/Corrections/school.sql
@@ -4,9 +4,7 @@ CREATE TABLE `clients` (
     `address` TEXT
 ) ENGINE = InnoDB;
 
-
 -- Modification pour un autre exercice
-
 ALTER TABLE
     `clients`
 ADD
@@ -14,9 +12,84 @@ ADD
 AFTER
     `address`;
 
+ALTER TABLE
+    `clients`
+ADD
+    COLUMN `scores` JSON
+AFTER
+    `notes`;
+
 UPDATE
     `clients`
 SET
-    `notes` = ('{"note1": 16, "note2": 17, "note3" : 10 }')
-WHERE
-    `name` = 'Alan';
+    `notes` = '[
+            { "note" : 19, "m" : "a" },
+            { "note" : 17, "m" : "b" },
+            { "note" : 17, "m" : "f" },
+            { "note" : 15, "m" : "e" },
+            { "note" : 11, "m" : "d" }
+        ]',
+    `scores` = '[
+            19, 20, 18, 11, 10
+        ]';
+
+select
+    notes -> '$[*]'
+from
+    clients;
+
+-- requete spécifique
+
+"""
+JSON_TABLE crée une table à partir d'un champ de type JSON
+$ désigne le document JSON
+
+"""
+
+SELECT
+    ROUND(AVG(`j`.`n`), 2) as `avg`,
+    `c`.`name`,
+    `c`.`id`
+FROM
+    `clients` as `c`
+    JOIN JSON_TABLE(
+        `c`.`scores`,
+        '$[*]' columns(
+            `n` decimal(8, 2) path '$'
+        )
+    ) as `j`
+GROUP BY
+    `c`.`id`;
+
+SELECT
+    ROUND(AVG(j.n), 2) as avg_note,
+    id, name, address
+FROM
+    clients as c
+    JOIN json_table(
+        c.scores,
+        '$[*]' columns(
+            n decimal(8, 2) path '$'
+        )
+    ) as j
+GROUP BY
+    id;
+
+set
+    @json = '
+[
+  {"name":"Laptop", "color":"black", "price":"1000"},
+  {"name":"Jeans",  "color":"blue"}
+]';
+
+select
+    *
+from
+    json_table(
+        @json,
+        '$[*]' columns(
+            name varchar(10) path '$.name',
+            color varchar(10) path '$.color',
+            price decimal(8, 2) path '$.price'
+        )
+    ) as jt;
---
+++ TALEND/chap2_api.md
@@ -0,0 +1,38 @@
+# API
+
+## Récupération des données
+
+```txt
+https://restcountries.com/v2/callingcode/33
+```
+
+## 1. Créez un fichier JSON dans la partie meta
+
+Formatez vos données en récupérant uniquement le nom du Pays, de la monnaie, de son symbole et le code.
+
+## 2. tREST
+
+Ce composant permet de récupérer des données REST. Vous allez le configurer pour récupérer les données à l'aide de la méthode GET.
+
+**https://restcountries.com/v2/callingcode/33**
+
+## 3. Vérifiez que les données sont bien celles que l'on attendait.
+
+Créez un tLogRow pour afficher les données.
+
+## 4. Créez un fichier JSON qui récupère les données que l'on vient d'afficher
+
+Utilisez le composant tFileOutPutJson. On aimerait récupérer les données suivantes
+
+```json
+{
+    "data": [
+        {
+            "symbol": "\u20AC",
+            "code": "EUR",
+            "CountryName": "[\"France\"]",
+            "name": "Euro"
+        }
+    ]
+}
+```
